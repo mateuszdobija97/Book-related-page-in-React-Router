48baf1b32e91cc6d3ed797796966c129421db5dc
Mateusz Dobija <mateusz.dobija.rce@gmail.com>
2020-05-06 17:54:50 +0200

    Updates

---
+++ precache-manifest.356b4887729741f9f59b9c817de0ee5e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc66b2d90614b92be858fb83a06e7737",
+    "url": "/Book-related-page-in-React-Router/index.html"
+  },
+  {
+    "revision": "90f0fff1c837e30e429c",
+    "url": "/Book-related-page-in-React-Router/static/css/main.6c7eab83.chunk.css"
+  },
+  {
+    "revision": "b68fda0c2cf3ca3f8cc9",
+    "url": "/Book-related-page-in-React-Router/static/js/2.365fca5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Book-related-page-in-React-Router/static/js/2.365fca5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f0fff1c837e30e429c",
+    "url": "/Book-related-page-in-React-Router/static/js/main.6808cbb9.chunk.js"
+  },
+  {
+    "revision": "dbba49dfbbcedbeec64b",
+    "url": "/Book-related-page-in-React-Router/static/js/runtime-main.fa7e0233.js"
+  },
+  {
+    "revision": "ddbe7d64126decb2a532142869e93195",
+    "url": "/Book-related-page-in-React-Router/static/media/book1.ddbe7d64.PNG"
+  },
+  {
+    "revision": "3a687f552b237f957b76ddfe4b789ba1",
+    "url": "/Book-related-page-in-React-Router/static/media/book2.3a687f55.PNG"
+  },
+  {
+    "revision": "2e37c064d88bef8e97bdeb380f748fb0",
+    "url": "/Book-related-page-in-React-Router/static/media/book3.2e37c064.PNG"
+  },
+  {
+    "revision": "9fde2ec67ed0a975e799cbe4b2ae242b",
+    "url": "/Book-related-page-in-React-Router/static/media/book4.9fde2ec6.PNG"
+  },
+  {
+    "revision": "8caaa4b80cbd447f1026fd47b02d72f3",
+    "url": "/Book-related-page-in-React-Router/static/media/book5.8caaa4b8.PNG"
+  }
+]);
